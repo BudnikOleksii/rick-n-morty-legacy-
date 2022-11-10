48080bdfa94580b67d5908b81ca8dc8de7dcfeb1
Oleksii Budnik <budnikoleksii@gmail.com>
2022-11-10 13:34:52 +0200

    remove unncessary functions and change methods, add characters_episodes table seed

--- server/db/migrations/20221108113142_create_characters_episodes.js
+++ server/db/migrations/20221108113142_create_characters_episodes.js
@@ -1,23 +1,23 @@
 exports.up = function(knex) {
   return knex.schema.createTable('characters_episodes', (table) => {
     table
-      .integer('episode_id')
+      .integer('character_id')
       .unsigned()
       .notNullable();
     table
-      .integer('character_id')
+      .integer('episode_id')
       .unsigned()
       .notNullable();
 
     table
-      .foreign('episode_id')
-      .references('id')
-      .inTable('episodes')
-      .onDelete('cascade');
-    table
       .foreign('character_id')
       .references('id')
       .inTable('characters')
+      .onDelete('cascade');
+    table
+      .foreign('episode_id')
+      .references('id')
+      .inTable('episodes')
       .onDelete('cascade');
   });
 };
---
+++ server/db/seeds/05_characters_episodes.js
@@ -0,0 +1,39 @@
+const { getCharactersData } = require('../helpersAPI/get-characters-data');
+const { getEpisodesData } = require('../helpersAPI/get-episodes-data');
+const { getIdFromUrl } = require('../helpersAPI/get-id-from-url');
+
+exports.seed = async function(knex) {
+  await knex('characters_episodes').del();
+
+  const charactersEpisodesData = [];
+  const episodesIdsMap = new Map();
+  const charactersIdsMap = new Map();
+
+  const { charactersEpisodes } = await getCharactersData();
+  const { episodesNamesMap } = await  getEpisodesData();
+
+  (await knex.select('id', 'name')
+    .from('episodes'))
+    .forEach(data => {
+      episodesIdsMap.set(data.name, data.id);
+    });
+
+  (await knex.select('id', 'name')
+    .from('characters'))
+    .forEach(data => {
+      charactersIdsMap.set(data.name, data.id);
+    });
+
+  charactersEpisodes.forEach(characterData => {
+    characterData[1].forEach(episodeUrl => {
+      const episodeName = episodesNamesMap.get(getIdFromUrl(episodeUrl));
+
+      charactersEpisodesData.push({
+        character_id: charactersIdsMap.get(characterData[0]),
+        episode_id: episodesIdsMap.get(episodeName),
+      })
+    })
+  });
+
+  await knex('characters_episodes').insert(charactersEpisodesData);
+};
