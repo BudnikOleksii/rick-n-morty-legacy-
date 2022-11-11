9921fd3a43453514c27919ac081f791fdadcfdb9
Oleksii Budnik <budnikoleksii@gmail.com>
2022-11-11 18:33:36 +0200

    added error middleware handler

--- server/src/middlewares/error-handler.js
+++ server/src/middlewares/error-handler.js
@@ -1,10 +1,9 @@
-const BaseError = require('../utils/errors/BaseError');
 const { Api500Error } = require('../utils/errors/ApiErrors');
 
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
   let error = err;
 
-  if (!(error instanceof BaseError)) {
+  if (!error.statusCode) {
     error = new Api500Error('Something went wrong');
   }
 
