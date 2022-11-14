8530808865dbb9359421259fbe2678c906c67032
Oleksii Budnik <budnikoleksii@gmail.com>
2022-11-14 11:54:41 +0200

    add route for all users, add model for users_roles

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { httpGetUserById } = require('../controllers/users');
+const { httpGetUserById, httpGetAllUsers } = require('../controllers/users');
 
 const usersRouter = express.Router();
 
+usersRouter.get('/', httpGetAllUsers);
 usersRouter.get('/:id', httpGetUserById);
 
 module.exports = usersRouter;
