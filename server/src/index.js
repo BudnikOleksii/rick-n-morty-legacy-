b5076f0325d247dba719d5562aceffa467b72fc4
Oleksii Budnik <budnikoleksii@gmail.com>
2022-12-01 11:49:38 +0200

    add chat websocket connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,26 @@
 const http = require('http');
 
-const { port } = require('../config').server;
+const { port, socketEvents } = require('../config').server;
 const app = require('./app');
 const { LotsService } = require('./services/lots');
 const { initCronJobs } = require('./cron-jobs');
 
 const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  socket.on(socketEvents.join, ({ roomId, userName }) => {
+    // Leave all rooms before join another one
+    socket.rooms.forEach(room => socket.leave(room));
+    socket.join(roomId);
+    socket.to(roomId).emit(socketEvents.receive, `${userName} just join room number ${roomId}`);
+  });
+
+  socket.on(socketEvents.send, (data) => {
+    // With data send room id to notify other users in current chat
+    socket.to(data.roomId).emit(socketEvents.receive, data.msg);
+  });
+});
 
 const startServer = async () => {
   await LotsService.closeAllFinishedAuctions();
