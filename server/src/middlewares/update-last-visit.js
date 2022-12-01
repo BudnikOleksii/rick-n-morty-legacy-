73b2b63e0239da5d48130a4fe2f1d0c15ddc11b6
Oleksii Budnik <budnikoleksii@gmail.com>
2022-12-01 14:01:44 +0200

    remove unneccessary next calls

--- server/src/middlewares/update-last-visit.js
+++ server/src/middlewares/update-last-visit.js
@@ -1,17 +1,12 @@
 const { UserService } = require('../services/users');
 const { getIpFromRequest } = require('../utils/get-ip-from-request');
-const updateLastVisitDate = async (req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    next();
-  }
 
+const updateLastVisitDate = async (req, res, next) => {
   if (req.user) {
     try {
       const ip = getIpFromRequest(req);
 
       await UserService.updateLastSeen(req.user.id, ip);
-
-      return next();
     } catch (error) {
       return next(error);
     }
