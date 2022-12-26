0a3ff051db3106e5fde7bc187c815153df9e503d
Oleksii Budnik <budnikoleksii@gmail.com>
2022-12-26 11:28:56 +0200

    tests with db

--- server/db/migrations/20221107160008_create_users_table.js
+++ server/db/migrations/20221107160008_create_users_table.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments('id');
     table.string('username').notNullable().unique();
@@ -8,11 +8,11 @@ exports.up = function(knex) {
     table.timestamp('registration_date').defaultTo(knex.fn.now());
     table.timestamp('last_visit_date').defaultTo(knex.fn.now());
     table.string('ip').notNullable();
-    table.boolean('activated').defaultTo(true);
+    table.boolean('activated').defaultTo(false);
     table.timestamp('deleted_at').nullable().defaultTo(null);
   });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTable('users');
 };
--- server/db/migrations/20221223135250_add_stripe_account_id_column_to_users_table.js
+++ server/db/migrations/20221223135250_add_stripe_account_id_column_to_users_table.js
@@ -5,11 +5,13 @@
 exports.up = function (knex) {
   return knex.schema.table('users', (table) => {
     table.string('stripe_account_id', 128);
+    table.string('activation_link', 128);
   });
 };
 
 exports.down = function (knex) {
   return knex.schema.table('users', (table) => {
     table.dropColumn('stripe_account_id');
+    table.dropColumn('activation_link');
   });
 };
